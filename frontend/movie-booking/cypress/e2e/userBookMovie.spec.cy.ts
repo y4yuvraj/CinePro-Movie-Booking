e4b5515a45040d122eda8b7540a290375b15cbbe
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2023-11-09 15:20:25 +0530

    App final changes

--- frontend/movie-booking/cypress/e2e/userBookMovie.spec.cy.ts
+++ frontend/movie-booking/cypress/e2e/userBookMovie.spec.cy.ts
@@ -3,16 +3,16 @@ describe('User Booking Movie', () => {
         cy.login('johndoe2', 'pw2');
     });
   
-    // it('should allow a user to book a movie', () => {
-    //   cy.visit('http://localhost:4200/user-dashboard/user-view-movies');
-    //   cy.wait(2000);
-    //   cy.get('.mat-elevation-z8 tbody tr:first-of-type button:contains("Book")').click();
-    //   cy.get('.swal2-popup input[type="number"]').type('2');
-    //   cy.get('.swal2-popup button:contains("Book")').click();
-    //   cy.get('.mat-snack-bar-container > [aria-hidden="true"]').should('be.visible')
-    //   cy.get('.mat-snack-bar-container > [aria-hidden="true"]').should('contain','Tickets booked')
-    //   cy.get('[data-cy="logout-button"]').click();
-    // });
+    it('should allow a user to book a movie', () => {
+      cy.visit('http://localhost:4200/user-dashboard/user-view-movies');
+      cy.wait(2000);
+      cy.get('.mat-elevation-z8 tbody tr:first-of-type button:contains("Book")').click();
+      cy.get('.swal2-popup input[type="number"]').type('2');
+      cy.get('.swal2-popup button:contains("Book")').click();
+      cy.get('.mat-snack-bar-container > [aria-hidden="true"]').should('be.visible')
+      cy.get('.mat-snack-bar-container > [aria-hidden="true"]').should('contain','Tickets booked')
+      cy.get('[data-cy="logout-button"]').click();
+    });
   
     it('should show an error message when booking fails', () => {
       cy.visit('http://localhost:4200/user-dashboard/user-view-movies');
